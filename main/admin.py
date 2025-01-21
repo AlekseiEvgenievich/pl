5b14c2b3c3bdfec44480ddc907b5458f74cdee3d
AlekseiEvgenievich <aleksei.smirnov@phystech.edu>
2025-01-21 19:00:13 +0100

    5task

--- main/admin.py
+++ main/admin.py
@@ -3,11 +3,18 @@ from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from ckeditor.widgets import CKEditorWidget
 from django.db import models
+from .models import Seller, Category, Tag, Ad
 
 class FlatPageCustomAdmin(FlatPageAdmin):
     formfield_overrides = {
         models.TextField: {'widget': CKEditorWidget},
     }
 
+
 admin.site.unregister(FlatPage)  # Убираем стандартную регистрацию
-admin.site.register(FlatPage, FlatPageCustomAdmin)  # Регистрируем с новым классом
+# Регистрируем с новым классом
+admin.site.register(FlatPage, FlatPageCustomAdmin)
+admin.site.register(Seller)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Ad)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Seller(models.Model):  # Исправлено с model.Model на models.Model
+    # Связь один-к-одному с пользователем
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="seller")
+
+    @property
+    def published_ads_count(self):
+        # В данном примере возвращаем фиксированное значение
+        return 42
+
+
+class Category(models.Model):
+	name = models.SlugField(default="", null=False)
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=255)
+
+
+class Ad(models.Model):  # Исправлено с model.Model на models.Model
+    name = models.CharField(max_length=255)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
